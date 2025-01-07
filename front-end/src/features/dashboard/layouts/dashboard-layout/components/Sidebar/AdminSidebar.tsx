e551cb7d068848ee29af21efdddb63939d0abf7e
the.tranthanh <phonvan128@gmail.com>
2025-01-08 01:51:32 +0700

    Update code

--- front-end/src/features/dashboard/layouts/dashboard-layout/components/Sidebar/AdminSidebar.tsx
+++ front-end/src/features/dashboard/layouts/dashboard-layout/components/Sidebar/AdminSidebar.tsx
@@ -1,3 +1,8 @@
+import ProtectByPremissions from '@/components/ProtectByPremissions';
+import RoleBasedComponent from '@/components/RoleBasedComponent';
+import useHasPermissons from '@/hooks/useHasPermissons';
+import { PermissionEnum } from '@/types/enum/account.enum';
+import { ROLE } from '@/utils/constants/constants';
 import AllInboxIcon from '@mui/icons-material/AllInbox';
 import CategoryIcon from '@mui/icons-material/Category';
 import FingerprintIcon from '@mui/icons-material/Fingerprint';
@@ -37,6 +42,26 @@ export default function AdminSidebar() {
     console.log('location.pathname', location.pathname);
   }, [location.pathname]);
 
+  const accessCourse = useHasPermissons({
+    permissions: [
+      PermissionEnum.CREATE_COURSE,
+      PermissionEnum.READ_COURSE,
+      PermissionEnum.UPDATE_COURSE,
+      PermissionEnum.DELETE_COURSE,
+    ],
+    needAll: false,
+  });
+
+  const accessPratice = useHasPermissons({
+    permissions: [
+      PermissionEnum.CREATE_PRACTICE,
+      PermissionEnum.READ_PRACTICE,
+      PermissionEnum.UPDATE_PRACTICE,
+      PermissionEnum.DELETE_PRACTICE,
+    ],
+    needAll: false,
+  });
+
   return (
     <div style={{ display: 'flex', height: '100%' }}>
       <Sidebar>
@@ -68,90 +93,157 @@ export default function AdminSidebar() {
           >
             Tin nhắn
           </MenuItem>
-          <SubMenu
-            defaultOpen={location.pathname.startsWith(EAppPath.COURSES)}
-            label="Khóa học"
-            icon={<MenuBookIcon />}
-            active={location.pathname.startsWith(EAppPath.COURSES)}
+
+          <ProtectByPremissions
+            permissions={[
+              PermissionEnum.CREATE_COURSE,
+              PermissionEnum.READ_COURSE,
+              PermissionEnum.UPDATE_COURSE,
+              PermissionEnum.DELETE_COURSE,
+            ]}
+            needAll={false}
+          >
+            <SubMenu
+              defaultOpen={location.pathname.startsWith(EAppPath.COURSES)}
+              label="Khóa học"
+              icon={<MenuBookIcon />}
+              active={location.pathname.startsWith(EAppPath.COURSES)}
+            >
+              <MenuItem
+                icon={<FormatListBulletedIcon />}
+                active={location.pathname.startsWith(EAppPath.COURSES_LIST)}
+                onClick={() => navigate(EAppPath.COURSES_LIST)}
+              >
+                Danh sách khóa học
+              </MenuItem>
+              <MenuItem
+                icon={<CategoryIcon />}
+                active={location.pathname.startsWith(EAppPath.COURSES_CATEGORY)}
+                onClick={() => navigate(EAppPath.COURSES_CATEGORY)}
+              >
+                Danh mục
+              </MenuItem>
+            </SubMenu>
+          </ProtectByPremissions>
+          <ProtectByPremissions
+            permissions={[
+              PermissionEnum.CREATE_PRACTICE,
+              PermissionEnum.READ_PRACTICE,
+              PermissionEnum.UPDATE_PRACTICE,
+              PermissionEnum.DELETE_PRACTICE,
+            ]}
+            needAll={false}
+          >
+            <SubMenu
+              defaultOpen={location.pathname.startsWith(EAppPath.PRACTICES)}
+              label="Luyện tập"
+              icon={<SchoolIcon />}
+              active={location.pathname.startsWith(EAppPath.PRACTICES)}
+            >
+              <MenuItem
+                icon={<FormatListBulletedIcon />}
+                active={location.pathname.startsWith(EAppPath.PRACTICES_LIST)}
+                onClick={() => navigate(EAppPath.PRACTICES_LIST)}
+              >
+                Danh sách luyện tập
+              </MenuItem>
+              <MenuItem
+                icon={<CategoryIcon />}
+                active={location.pathname.startsWith(
+                  EAppPath.PRACTICES_CATEGORY,
+                )}
+                onClick={() => navigate(EAppPath.PRACTICES_CATEGORY)}
+              >
+                Quản lý nộp bài
+              </MenuItem>
+            </SubMenu>
+          </ProtectByPremissions>
+          <ProtectByPremissions
+            permissions={[PermissionEnum.ACCESS_PAYMENT]}
+            needAll={true}
           >
             <MenuItem
-              icon={<FormatListBulletedIcon />}
-              active={location.pathname.startsWith(EAppPath.COURSES_LIST)}
-              onClick={() => navigate(EAppPath.COURSES_LIST)}
+              icon={<PaymentIcon />}
+              active={location.pathname.startsWith(EAppPath.PAYMENTS)}
+              onClick={() => navigate(EAppPath.PAYMENTS)}
             >
-              Danh sách khóa học
+              Hóa đơn
             </MenuItem>
-            <MenuItem
-              icon={<CategoryIcon />}
-              active={location.pathname.startsWith(EAppPath.COURSES_CATEGORY)}
-              onClick={() => navigate(EAppPath.COURSES_CATEGORY)}
-            >
-              Danh mục
-            </MenuItem>
-          </SubMenu>
-          <SubMenu
-            defaultOpen={location.pathname.startsWith(EAppPath.PRACTICES)}
-            label="Luyện tập"
-            icon={<SchoolIcon />}
-            active={location.pathname.startsWith(EAppPath.PRACTICES)}
+          </ProtectByPremissions>
+          <ProtectByPremissions
+            permissions={[PermissionEnum.ACCESS_STATISTIC]}
+            needAll={true}
           >
             <MenuItem
-              icon={<FormatListBulletedIcon />}
-              active={location.pathname.startsWith(EAppPath.PRACTICES_LIST)}
-              onClick={() => navigate(EAppPath.PRACTICES_LIST)}
+              icon={<InsightsIcon />}
+              active={location.pathname.startsWith(EAppPath.INSIGHTS)}
+              onClick={() => navigate(EAppPath.INSIGHTS)}
             >
-              Danh sách luyện tập
+              Thống kê
             </MenuItem>
-            <MenuItem
-              icon={<CategoryIcon />}
-              active={location.pathname.startsWith(EAppPath.PRACTICES_CATEGORY)}
-              onClick={() => navigate(EAppPath.PRACTICES_CATEGORY)}
-            >
-              Quản lý nộp bài
-            </MenuItem>
-          </SubMenu>
-          <MenuItem
-            icon={<PaymentIcon />}
-            active={location.pathname.startsWith(EAppPath.PAYMENTS)}
-            onClick={() => navigate(EAppPath.PAYMENTS)}
+          </ProtectByPremissions>
+          <ProtectByPremissions
+            permissions={[
+              PermissionEnum.CREATE_USER,
+              PermissionEnum.READ_USER,
+              PermissionEnum.UPDATE_USER,
+              PermissionEnum.DELETE_USER,
+              PermissionEnum.CREATE_ROLE,
+              PermissionEnum.READ_ROLE,
+              PermissionEnum.UPDATE_ROLE,
+              PermissionEnum.DELETE_ROLE,
+            ]}
+            needAll={false}
           >
-            Hóa đơn
-          </MenuItem>
-          <MenuItem
-            icon={<InsightsIcon />}
-            active={location.pathname.startsWith(EAppPath.INSIGHTS)}
-            onClick={() => navigate(EAppPath.INSIGHTS)}
-          >
-            Thống kê
-          </MenuItem>
-          <SubMenu
-            defaultOpen={location.pathname.startsWith(EAppPath.MAGAGER)}
-            label="Quản lý người dùng"
-            icon={<GroupsIcon />}
-            active={location.pathname.startsWith(EAppPath.MAGAGER)}
-          >
-            <MenuItem
-              icon={<PersonIcon />}
-              active={location.pathname.startsWith(EAppPath.MANAGE_STUDENT)}
-              onClick={() => navigate(EAppPath.MANAGE_STUDENT)}
+            <SubMenu
+              defaultOpen={location.pathname.startsWith(EAppPath.MAGAGER)}
+              label="Quản lý người dùng"
+              icon={<GroupsIcon />}
+              active={location.pathname.startsWith(EAppPath.MAGAGER)}
             >
-              Quản lý học sinh
-            </MenuItem>
-            <MenuItem
-              icon={<ManageAccountsIcon />}
-              active={location.pathname.startsWith(EAppPath.MANAGE_STAFF)}
-              onClick={() => navigate(EAppPath.MANAGE_STAFF)}
-            >
-              Quản lý nhân viên
-            </MenuItem>
-            <MenuItem
-              icon={<FingerprintIcon />}
-              active={location.pathname.startsWith(EAppPath.MANAGE_ROLE)}
-              onClick={() => navigate(EAppPath.MANAGE_ROLE)}
-            >
-              Quản lý quản lý role
-            </MenuItem>
-          </SubMenu>
+              <ProtectByPremissions
+                permissions={[
+                  PermissionEnum.CREATE_USER,
+                  PermissionEnum.READ_USER,
+                  PermissionEnum.UPDATE_USER,
+                  PermissionEnum.DELETE_USER,
+                ]}
+                needAll={false}
+              >
+                <MenuItem
+                  icon={<PersonIcon />}
+                  active={location.pathname.startsWith(EAppPath.MANAGE_STUDENT)}
+                  onClick={() => navigate(EAppPath.MANAGE_STUDENT)}
+                >
+                  Quản lý học sinh
+                </MenuItem>
+                <MenuItem
+                  icon={<ManageAccountsIcon />}
+                  active={location.pathname.startsWith(EAppPath.MANAGE_STAFF)}
+                  onClick={() => navigate(EAppPath.MANAGE_STAFF)}
+                >
+                  Quản lý nhân viên
+                </MenuItem>
+              </ProtectByPremissions>
+              <ProtectByPremissions
+                permissions={[
+                  PermissionEnum.CREATE_ROLE,
+                  PermissionEnum.READ_ROLE,
+                  PermissionEnum.UPDATE_ROLE,
+                  PermissionEnum.DELETE_ROLE,
+                ]}
+                needAll={false}
+              >
+                <MenuItem
+                  icon={<FingerprintIcon />}
+                  active={location.pathname.startsWith(EAppPath.MANAGE_ROLE)}
+                  onClick={() => navigate(EAppPath.MANAGE_ROLE)}
+                >
+                  Quản lý quản lý role
+                </MenuItem>
+              </ProtectByPremissions>
+            </SubMenu>
+          </ProtectByPremissions>
         </Menu>
       </Sidebar>
     </div>

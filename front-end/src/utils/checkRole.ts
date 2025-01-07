34c55ca55612779e36778350dd479bad67dcd307
the.tranthanh <phonvan128@gmail.com>
2025-01-08 01:18:54 +0700

    Update code

--- front-end/src/utils/checkRole.ts
+++ front-end/src/utils/checkRole.ts
@@ -4,5 +4,6 @@ export const checkRole = (
   userRole: Role | null,
   allowedRoles: Role[],
 ): boolean => {
+  if (userRole !== 'User' && userRole !== 'Admin') userRole = 'Staff';
   return allowedRoles.includes(userRole as Role);
 };
